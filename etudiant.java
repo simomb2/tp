05348542b01bf13ae1b2fd834e5eb5d727b8c522
moatadidbillah <moatadidbillah2001@gmail.com>
2024-02-21 22:20:12 +0100

    first commit in branch featurestats

--- etudiant.java
+++ etudiant.java
@@ -4,4 +4,17 @@ public class Etudiant {
     private String prenom;
     private int note;
     private double notes;
+    
+    public static double calculerMoyenne(List<Etudiant> listeEtudiants) {
+        if (listeEtudiants == null || listeEtudiants.isEmpty()) {
+            System.out.println("Aucun étudiant dans la liste.");
+            return 0.0;
+        }
+        double sommeNotes = 0.0;
+        int nombreEtudiants = listeEtudiants.size();
+        for (Etudiant etudiant : listeEtudiants) {
+            sommeNotes += etudiant.getNoteMoyenne();
+        }
+        return sommeNotes / nombreEtudiants;
+    }
 }
